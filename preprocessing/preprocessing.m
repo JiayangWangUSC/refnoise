ab3e46993e238f68cc62ca890818e237cf1a3888
JiayangWang <jiayangwQ@usc.edu>
2022-08-26 13:15:26 -0700

    preprocessing

--- preprocessing/preprocessing.m
+++ preprocessing/preprocessing.m
@@ -4,10 +4,10 @@ dirname = dir(datapath);
 N1 = 384; N2 = 396; Nc = 16; Ns = 8;
 
 %%
-newdatapath = '/project/jhaldar_118/jiayangw/dataset/brain_clean/train/';
-for dir_num = 3:length(dirname)
-    h5create([newdatapath,dirname(dir_num).name],'/kspace',[Ns,N1,N2,Nc,2]);
-end
+%newdatapath = '/project/jhaldar_118/jiayangw/dataset/brain_clean/train/';
+%for dir_num = 3:length(dirname)
+%    h5create([newdatapath,dirname(dir_num).name],'/kspace',[Ns,N1,N2,Nc,2]);
+%end
 
 %%
 fft2c = @(x) fftshift(fft2(ifftshift(x)))/sqrt(size(x(:),1))*4;
