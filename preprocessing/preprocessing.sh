99fc2c88f548dea33c78d3e233ae9c2086889d35
JiayangWang <jiayangwQ@usc.edu>
2022-09-02 18:45:23 -0700

    preprocessing

--- preprocessing/preprocessing.sh
+++ preprocessing/preprocessing.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=16GB
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jiayangw@usc.edu
 #SBATCH --account=jhaldar_118
