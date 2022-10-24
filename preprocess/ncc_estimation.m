ab4458383a0804ff699e653de9fd3155bbe5309c
JiayangWang <jiayangwQ@usc.edu>
2022-10-24 12:17:04 -0700

    preprocess

--- preprocess/ncc_estimation.m
+++ preprocess/ncc_estimation.m
@@ -1,5 +1,5 @@
 %% effective NCC noise parameters estimation
-datapath = '/project/jhaldar_118/jiayangw/dataset/brain_cppy/train/';
+datapath = '/project/jhaldar_118/jiayangw/dataset/brain_copy/train/';
 %datapath = '/home/wjy/Project/fastmri_dataset/miniset_brain_clean/';
 dirname = dir(datapath);
 N1 = 384; N2 = 396; Nc = 16; Ns = 8;
