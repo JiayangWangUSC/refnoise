ebd213eea61b0eac24ec0f81581e85279cc2b296
JiayangWang <jiayangwQ@usc.edu>
2022-11-02 15:08:25 -0700

    mae unet

--- noisy_imunet.py
+++ noisy_imunet.py
@@ -86,7 +86,7 @@ for epoch in range(max_epochs):
         recon = torch.cat((image_output[:,torch.arange(nc),:,:].unsqueeze(4),image_output[:,torch.arange(nc,2*nc),:,:].unsqueeze(4)),4).to(device)
         recon = fastmri.rss(fastmri.complex_abs(recon),dim=1)
 
-        loss = L2Loss(recon.to(device),gt.to(device))
+        loss = L1Loss(recon.to(device),gt.to(device))
     
         if batch_count%100 == 0:
             print("batch:",batch_count,"train loss:",loss.item())
@@ -95,4 +95,4 @@ for epoch in range(max_epochs):
         recon_optimizer.step()
         recon_optimizer.zero_grad()
     if (epoch + 1)%20 == 0:
-        torch.save(recon_model,"/project/jhaldar_118/jiayangw/refnoise/model/imunet_mse_acc4_epoch"+str(epoch+1))
+        torch.save(recon_model,"/project/jhaldar_118/jiayangw/refnoise/model/imunet_mae_acc4_epoch"+str(epoch+1))
