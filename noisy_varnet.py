c18daff06e08ffdb2ff9c40cf04da2008b4533e1
JiayangWang <jiayangwQ@usc.edu>
2023-01-19 15:03:42 -0800

    modl ncc acc4

--- noisy_varnet.py
+++ noisy_varnet.py
@@ -69,7 +69,7 @@ L1Loss = torch.nn.L1Loss()
 
 # %% sampling mask
 mask = torch.zeros(ny)
-mask[torch.arange(132)*3] = 1
+mask[torch.arange(66)*6] = 1
 mask[torch.arange(186,210)] =1
 mask = mask.bool().unsqueeze(0).unsqueeze(0).unsqueeze(3).repeat(nc,nx,1,2)
 
@@ -96,6 +96,6 @@ for epoch in range(max_epochs):
         recon_optimizer.step()
         recon_optimizer.zero_grad()
     #if (epoch + 1)%20 == 0:
-    torch.save(recon_model,"/project/jhaldar_118/jiayangw/refnoise/model/varnet_mae_acc3")
+    torch.save(recon_model,"/project/jhaldar_118/jiayangw/refnoise/model/varnet_mae_acc6")
 
 # %%
