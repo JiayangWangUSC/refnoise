46e0af8976f089bf15fb08d50ca5e752876d52ab
JiayangWang <jiayangwQ@usc.edu>
2023-05-05 19:23:24 -0700

    1

--- noisy_modl.py
+++ noisy_modl.py
@@ -55,7 +55,7 @@ recon_model = MoDL(
     n_layers = layers,
     k_iters = iters
 )
-recon_modol = torch.load("/project/jhaldar_118/jiayangw/refnoise/model/modl_mse_acc3")
+recon_modol = torch.load("/project/jhaldar_118/jiayangw/refnoise/model/modl_mae_acc3")
 
 
 # %% training settings
@@ -91,7 +91,7 @@ for epoch in range(max_epochs):
         #recon = fastmri.complex_mul(sense_maps.to(device),torch.permute(recon,(0,2,3,1)))
         #recon = fastmri.rss(fastmri.complex_abs(recon),dim=1)
 
-        loss = L2Loss(recon.to(device),gt.to(device))
+        loss = L1Loss(recon.to(device),gt.to(device))
 
         if batch_count%100 == 0:
             print("batch:",batch_count,"train loss:",loss.item())
@@ -101,7 +101,7 @@ for epoch in range(max_epochs):
         recon_optimizer.zero_grad()
 
     
-    torch.save(recon_model,"/project/jhaldar_118/jiayangw/refnoise/model/modl_mse_acc3")
+    torch.save(recon_model,"/project/jhaldar_118/jiayangw/refnoise/model/modl_mae_acc3")
 
 # %%
 
