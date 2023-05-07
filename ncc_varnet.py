2cadae5bb193a28e0e52df8f35fb63208059be60
JiayangWang <jiayangwQ@usc.edu>
2023-05-06 18:32:49 -0700

    1

--- ncc_varnet.py
+++ ncc_varnet.py
@@ -57,7 +57,7 @@ recon_model = VarNet(
     pools = 4,
     mask_center= True
 )
-recon_model = torch.load("/project/jhaldar_118/jiayangw/refnoise/model/varnet_ncc_acc4")
+recon_model = torch.load("/project/jhaldar_118/jiayangw/refnoise/model/varnet_ncc_acc6")
 # %% training settings
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 batch_size = 2
@@ -69,7 +69,7 @@ L1Loss = torch.nn.L1Loss()
 
 # %% sampling mask
 mask = torch.zeros(ny)
-mask[torch.arange(99)*4] = 1
+mask[torch.arange(66)*6] = 1
 mask[torch.arange(186,210)] =1
 mask = mask.bool().unsqueeze(0).unsqueeze(0).unsqueeze(3).repeat(nc,nx,1,2)
 
@@ -101,4 +101,4 @@ for epoch in range(max_epochs):
         recon_optimizer.zero_grad()
 
     #if (epoch + 1)%20 == 0:
-    torch.save(recon_model,"/project/jhaldar_118/jiayangw/refnoise/model/varnet_ncc_acc4")
+    torch.save(recon_model,"/project/jhaldar_118/jiayangw/refnoise/model/varnet_ncc_acc6")
